b1402d2b10d635fbd086cacb38264356fd3d65e5
Bekish-Kakish <bekzatik22.088@gmail.com>
2026-04-19 21:10:15 +0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export', // Обязательно для работы на GitHub Pages (статический экспорт)
+  basePath: '/NextLevel', // Имя твоего репозитория, чтобы пути к картинкам не ломались
+  images: {
+    unoptimized: true, // GitHub не умеет оптимизировать картинки на лету
+  },
 };
 
 export default nextConfig;
